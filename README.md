d1c28162e8ac14d98091d0cc0fc18d7348b308c4
Too-Naive <sometimes.naive@hotmail.com>
2018-09-29 23:41:26 +0800

    Fixed an issue where the file was not compressed * Add README.md * Add decrypt function

---
+++ README.md
@@ -0,0 +1,31 @@
+# Backup database file to google drive
+
+## Operating Environment
+
+Python 3.4 and above is required
+
+The following libraries are required:
+
+- cryptography
+- hashlib
+
+## Feature
+
+* Encrypt with AES_GCM
+* Once upload, the previous file will be deleted automatically
+
+## WARNING
+
+**You should always remember your key, or your data will be lost forever**
+
+## Install
+
+* Copy `config.ini.default` to `config.ini`
+* Configure database name which you want to backup in `config.ini`
+* Configure log level and encrypt key in `config.ini`
+* Download [GDrive](https://github.com/prasmussen/gdrive) to your `/usr/bin` directory and login
+* Use `cron` to run this program programmatically
+
+## Decrypt
+
+Simply run `python3 main.py <aes_filename>`
--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-import subprocess, time, gzip, shutil, os
+import subprocess, time, gzip, shutil, os, sys
 from libpy3 import Log
 from libpy3.Encrypt import encrypt_by_AES_GCM as AES_GCM
 from configparser import ConfigParser
@@ -26,34 +26,45 @@ class timestamp:
 	timestamp_ = ''
 	@staticmethod
 	def gen():
-		timestamp.timestamp_ = time.strftime('%Y%m%d%H%M%S')
+		timestamp.timestamp_ = time.strftime('_%Y%m%d_%H%M%S')
 		return timestamp.get()
 	@staticmethod
 	def get():
 		return timestamp.timestamp_
 
 def gensql(db_names: tuple or list, db_passwd: str, *, db_user: str = 'root'):
-	sub = subprocess.Popen(['mysqldump', '-u', db_user,'-p{}'.format(db_passwd), '--databases'] + [ db_name for db_name in db_names], stdout=subprocess.PIPE)
+	sub = subprocess.Popen(['mysqldump', '-u', db_user,'-p{}'.format(db_passwd), '--databases'] + [ db_name for db_name in db_names], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 	#sub.wait()
 	return sub.communicate()[0].decode('utf8')
 
 def bkup(db_names: tuple or list, db_passwd: str, *, db_user: str = 'root'):
 	with open('backupsql{}.sql'.format(timestamp.gen()), 'w') as fout:
 		fout.write(gensql(db_names, db_passwd, db_user=db_user))
-	with open('backupsql{}.sql'.format(timestamp.get()), 'rb') as fin, open('backupsql{}.sql.gz'.format(timestamp.get()), 'wb') as fout:
-		shutil.copyfileobj(fin, fout)
-	with open('backupsql{}.sql.gz'.format(timestamp.get()), 'rb') as fin, open('backupsql{}.sql'.format(timestamp.get()), 'w') as fout:
-		fout.write(AES_GCM().b64encrypt(fin.read()))
-	os.remove('backupsql{}.sql.gz'.format(timestamp.get()))
-	sub = subprocess.Popen(['gdrive', 'upload', 'bakupsql{}.sql'.format(timestamp.get())], stdout=subprocess.PIPE)
+	encrypt_sql('backupsql{}'.format(timestamp.get()))
+	sub = subprocess.Popen(['gdrive', 'upload', 'backupsql{}.sql.aes'.format(timestamp.get())], stdout=subprocess.PIPE)
 	#sub.wait()
 	return sub.communicate()[0].decode('utf8').split()[3]
 
 def del_file(file_id: str):
-	sub = subprocess.Popen(['gdrive', 'delete', file_id])
+	sub = subprocess.Popen(['gdrive', 'delete', file_id], stdout=subprocess.PIPE)
 	#sub.wait()
 	r = sub.communicate()[0].decode('utf8')
 	return 'Deleted' in r
+
+def encrypt_sql(timestamps: str):
+	with open('{}.sql'.format(timestamps), 'rb') as fin, gzip.open('{}.sql.gz'.format(timestamps), 'wb') as fout:
+		shutil.copyfileobj(fin, fout)
+	with open('{}.sql.gz'.format(timestamps), 'rb') as fin, open('{}.sql.aes'.format(timestamps), 'w') as fout:
+		fout.write(AES_GCM().b64encrypt(fin.read()))
+	os.remove('{}.sql.gz'.format(timestamps))
+
+def decrypt_sql(file_name: str):
+	with open(file_name, 'r') as fin, open('{}.gz'.format(file_name), 'wb') as fout:
+		fout.write(AES_GCM().b64decrypt(fin.read()))
+	with gzip.open('{}.gz'.format(file_name), 'rb') as gfin, open('{}.orign'.format(file_name), 'wb') as fout:
+		shutil.copyfileobj(gfin, fout)
+	os.remove('{}.gz'.format(file_name))
+	Log.info('decrypt sql file successful ({}.orign)'.format(file_name))
 
 def main():
 	config = ConfigParser()
@@ -61,18 +72,19 @@ def main():
 	if config.has_option('sql', 'fileid'):
 		fileid_ = config['sql']['fileid']
 	else:
-		config['sql']['fileid'] = ''
-		fileid_ = ''
-	Log.info('Backup process started')
+		config['sql']['fileid'], fileid_ = '', ''
+	#Log.info('Backup process started')
 	current_fileid = bkup(eval(config['sql']['databases']), config['sql']['passwd'])
+	os.remove('backupsql{}.sql'.format(timestamp.get()))
 	#print(repr(current_fileid))
 	if fileid_ != '':
-		del_file(fileid_)
+		try: del_file(fileid_)
+		except: Log.exc()
 	config['sql']['fileid'] = current_fileid
 	Log.info('Success backup {}', current_fileid)
-	with open('config.ini', 'w') as fout:
-		config.write(fout)
+	with open('config.ini', 'w') as fout: config.write(fout)
 
 if __name__ == '__main__':
-	main()
+	if len(sys.argv) == 2: decrypt_sql(sys.argv[1])
+	else: main()
 
