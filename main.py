db8b2dc92b9277c889393a9d2b34650d9a1b9249
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2018-12-31 04:17:30 +0800

    Add optional prefix setting * Fix mysqldump error if databases value is not a tuple * Update submodule location

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # main.py
-# Copyright (C) 2018 Too-Naive
+# Copyright (C) 2018 KunoiSayami
 #
 # This module is part of gdrive-backup and is released under
 # the AGPL v3 License: https://www.gnu.org/licenses/agpl-3.0.txt
@@ -23,6 +23,8 @@ from libpy3 import Log
 from libpy3.Encrypt import encrypt_by_AES_GCM as AES_GCM
 from configparser import ConfigParser
 
+global prefix
+
 class timestamp:
 	timestamp_ = ''
 	@staticmethod
@@ -34,30 +36,33 @@ class timestamp:
 		return timestamp.timestamp_
 
 def gensql(db_names: tuple or list, db_passwd: str, *, db_user: str = 'root'):
-	with open('backupsql{}.sql'.format(timestamp.gen()), 'w') as fout:
-		sub = subprocess.Popen(['mysqldump', '-u', db_user,'-p{}'.format(db_passwd), '--databases'] + [ db_name for db_name in db_names], stdout=fout, stderr=subprocess.DEVNULL)
-		sub.wait()
-	#return sub.communicate()[0].decode('utf8')
+	subprocess.Popen(
+		['mysqldump', '-u', db_user,'-p{}'.format(db_passwd), '--databases'] + [ db_name for db_name in db_names],
+		stdout = open('{}{}.sql'.format(prefix, timestamp.gen()), 'w'),
+		stderr = subprocess.DEVNULL,
+		close_fds = True
+	).wait()
 
 def bkup(db_names: tuple or list, db_passwd: str, *, db_user: str = 'root'):
-	#with open('backupsql{}.sql'.format(timestamp.gen()), 'w') as fout:
-	#	fout.write(gensql(db_names, db_passwd, db_user=db_user))
-	gensql(db_names, db_passwd, db_user=db_user)
-	encrypt_sql('backupsql{}'.format(timestamp.get()))
-	sub = subprocess.Popen(['gdrive', 'upload', 'backupsql{}.sql.aes'.format(timestamp.get())], stdout=subprocess.PIPE)
-	return sub.communicate()[0].decode('utf8').split()[3]
+	gensql(db_names, db_passwd, db_user = db_user)
+	encrypt_sql('{}{}'.format(prefix, timestamp.get()))
+	sub = subprocess.Popen(['gdrive', 'upload', '{}{}.sql.aes'.format(prefix, timestamp.get())], stdout = subprocess.PIPE)
+	r = sub.communicate()[0].decode('utf8')
+	if r.split()[3] == 'to':
+		Log.warn('File id is `to\': origin output:\n{}', repr(r))
+	return r.split()[3]
 
 def del_file(file_id: str):
-	sub = subprocess.Popen(['gdrive', 'delete', file_id], stdout=subprocess.PIPE)
+	sub = subprocess.Popen(['gdrive', 'delete', file_id], stdout = subprocess.PIPE)
 	r = sub.communicate()[0].decode('utf8')
 	return 'Deleted' in r
 
-def encrypt_sql(timestamps: str):
-	with open('{}.sql'.format(timestamps), 'rb') as fin, gzip.open('{}.sql.gz'.format(timestamps), 'wb') as fout:
+def encrypt_sql(filename_with_timestamp: str):
+	with open('{}.sql'.format(filename_with_timestamp), 'rb') as fin, gzip.open('{}.sql.gz'.format(filename_with_timestamp), 'wb') as fout:
 		shutil.copyfileobj(fin, fout)
-	with open('{}.sql.gz'.format(timestamps), 'rb') as fin, open('{}.sql.aes'.format(timestamps), 'w') as fout:
+	with open('{}.sql.gz'.format(filename_with_timestamp), 'rb') as fin, open('{}.sql.aes'.format(filename_with_timestamp), 'w') as fout:
 		fout.write(AES_GCM().b64encrypt(fin.read()))
-	os.remove('{}.sql.gz'.format(timestamps))
+	os.remove('{}.sql.gz'.format(filename_with_timestamp))
 
 def decrypt_sql(file_name: str):
 	with open(file_name, 'r') as fin, open('{}.gz'.format(file_name), 'wb') as fout:
@@ -68,16 +73,19 @@ def decrypt_sql(file_name: str):
 	Log.info('decrypt sql file successful ({}.orign)'.format(file_name))
 
 def main():
+	global prefix
 	config = ConfigParser()
 	config.read('config.ini')
 	if config.has_option('sql', 'fileid'):
 		fileid_ = config['sql']['fileid']
 	else:
 		config['sql']['fileid'], fileid_ = '', ''
+	prefix = config['sql']['prefix'] if config.has_option('sql', 'prefix') and config['sql']['prefix'] != '' else 'backupsql'
 	#Log.info('Backup process started')
+	if isinstance(eval(config['sql']['databases']), str): config['sql']['databases'] = repr((eval(config['sql']['databases']), ))
 	current_fileid = bkup(eval(config['sql']['databases']), config['sql']['passwd'])
-	os.remove('backupsql{}.sql'.format(timestamp.get()))
-	os.remove('backupsql{}.sql.aes'.format(timestamp.get()))
+	os.remove('{}{}.sql'.format(prefix, timestamp.get()))
+	os.remove('{}{}.sql.aes'.format(prefix, timestamp.get()))
 	#print(repr(current_fileid))
 	if fileid_ != '':
 		try: del_file(fileid_)
